0f51fdda5ee007b39c1dd1f0957c666fb1ba3651
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-02-16 12:38:40 +0530

    service worker registed
    listen to events, install activate, etc.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,7 +2,12 @@
 
 if ("serviceWorker" in navigator) {
     // register as a service worker 
-    navigator.serviceWorker.register("/sw.js").then(() => {
+    navigator.serviceWorker.register("/sw.js"
+    , {
+        scope: "/"
+        // higher scope doubt
+    }
+    ).then(() => {
         return console.log("Service worker registered")
     })
 }
---
+++ public/sw.js
@@ -0,0 +1,20 @@
+// refererring to service worker with self
+// no dom access to sw
+
+self.addEventListener("install", (event) => {
+    console.log("Install event", event)
+})
+
+
+self.addEventListener("activate", (event) => {
+    console.log("Activate event", event)
+
+    return self.clients.claim();
+})
+
+
+
+self.addEventListener("fetch", (event)=> {
+    console.log("Fetch event triggered", event)
+    event.respondWith(fetch(event.request))
+})
