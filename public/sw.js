2e775306087a7a59e59f3eb75c78a02739103929
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-02-16 16:26:37 +0530

    cache.addAll() fix 404 error from non repsonsive link

--- public/sw.js
+++ public/sw.js
@@ -9,9 +9,29 @@ self.addEventListener("install", (event) => {
     caches.open("static").then((cache) => {
       console.log("[Service Worker] Pre-cache..ing app.js ");
       //  cache..ing requests and initial request is root ( "/" )
-      cache.add("/");
-      cache.add("/index.html");
-      cache.add("/src/js/app.js");
+      // only needed on browsers that dont support service worke
+      // only loading here cause of performance reasons.
+      // can cache third party as well (they should have cors allow)
+
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
+
+      //   cache.add("/");
+      //   cache.add("/index.html");
+      //   cache.add("/src/js/app.js");
     })
   );
 });
