dae2c24efb5f1bf658b9975cd55d35828adcef3d
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-02-16 13:10:20 +0530

    add to homescreen install prompt

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,13 +1,24 @@
+var deferrredPrompt;
+
 // check service worker featuer available or not
 
 if ("serviceWorker" in navigator) {
-    // register as a service worker 
-    navigator.serviceWorker.register("/sw.js"
-    , {
-        scope: "/"
-        // higher scope doubt
-    }
-    ).then(() => {
-        return console.log("Service worker registered")
+  // register as a service worker
+  navigator.serviceWorker
+    .register("/sw.js", {
+      scope: "/",
+      // higher scope doubt
     })
+    .then(() => {
+      return console.log("Service worker registered");
+    });
 }
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  deferrredPrompt = event;
+  console.log("prompt prevent");
+  return false;
+});
+
+
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,15 +1,32 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
+
+  if (deferrredPrompt) {
+    deferrredPrompt.prompt();
+    deferrredPrompt.userChoice.then((choiceResult) => {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === "dismissed") {
+        console.log("user cancelled");
+      } else {
+        console.log("user added to home screen");
+      }
+    });
+
+    deferrredPrompt = null;
+  }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
